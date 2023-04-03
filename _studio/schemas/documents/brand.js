b025617fe46cd8b31bbf57f6986b55f1c8229d6b
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-04-03 11:01:51 +0200

    added brand document

---
+++ _studio/schemas/documents/brand.js
@@ -0,0 +1,17 @@
+export default {
+	title: 'Brand',
+	name: 'brand',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string',
+		} ,
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'image',
+		}
+	]
+}
