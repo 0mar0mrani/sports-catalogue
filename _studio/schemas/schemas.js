b4a2d6494a451b752d248df8ad7ec7fec2a94c33
Omar Omrani <95319854+0mar0mrani@users.noreply.github.com>
2023-04-03 11:53:55 +0200

    imported documents

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,1 +1,11 @@
-export default []
+import brand from './documents/brand.js';
+import category from './documents/category.js';
+import size from './documents/size.js';
+import product from './documents/product.js';
+
+export default [
+	brand,
+	category,
+	size,
+	product,
+]
